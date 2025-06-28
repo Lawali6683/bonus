efae3ed1a53cce0f2fc768dffa331dfe4761fb01
Haruna Lawali <lawaliharuna943@gmail.com>
2025-06-28 21:02:10 +0100

    Update email.js

--- api/email.js
+++ api/email.js
@@ -1,33 +1,32 @@
-const { MailerSend, EmailParams, Recipient } = require("mailersend");
+const MailerSend = require("mailersend");
+
+const mailersend = new MailerSend.MailerSend({
+  apiKey: process.env.MAILERSEND_API_KEY
+});
+
+const { EmailParams, Recipient } = MailerSend;
 
 module.exports = async (req, res) => {
   try {
     if (req.method !== "POST") {
-      console.log("❌ Invalid method used:", req.method);
       return res.status(405).json({ error: "Method Not Allowed" });
     }
 
     const { to, subject, htmlContent } = req.body;
 
     if (!to || !subject || !htmlContent) {
-      console.log("❌ Missing fields:", { to, subject, htmlContent });
       return res.status(400).json({ error: "Missing email details" });
     }
-
-    // API KEY (direct for testing)
-    const mailersend = new MailerSend({
-      apiKey: "mlsn.9bc72af91a8fdaad66f61dc9e4f8ec67b73e0729051bd36d323b66a75cd94ff2",
-    });
 
     const recipients = [new Recipient(to, "Recipient")];
 
     const emailParams = new EmailParams()
-      .setFrom("vestinoominer@gmai.com") 
+      .setFrom("vestinoomine@gmail.com")
       .setFromName("Vestinoo Team 📩")
       .setRecipients(recipients)
       .setSubject(subject)
       .setHtml(htmlContent)
-      .setText("Wannan sakon daga Vestinoo ne."); // optional plain text
+      .setText("This Email from Vestinoo Team.");
 
     const response = await mailersend.email.send(emailParams);
 
